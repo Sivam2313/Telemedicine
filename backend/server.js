ec006a1e73758619a1018487556b82dd07f5be9f
PrasantaScripts <mondalprasanta801@gmail.com>
2023-05-21 12:15:29 +0530

    added

--- backend/server.js
+++ backend/server.js
@@ -1,67 +1,65 @@
-const express = require('express');
-const dotenv = require('dotenv');
-dotenv.config({path:'../.env'});
-const connectDB = require('../backend/config/db');
-const adminRoutes = require('./routes/adminRoutes');
-const hwRoutes = require('./routes/hwRoutes');
-const doctorRoutes = require('./routes/doctorRoutes');
-const familyRoutes = require('./routes/familyRoutes');
-const patientRoutes = require('./routes/PatientRoutes');
-const logRoutes = require('./routes/logRoutes');
-const prescriptionRoutes = require('./routes/prescriptionRoutes')
-const medicineRoutes=require('./routes/medicineRoutes');
-const path = require('path');
-dotenv.config()
-const app = express()
-const PORT = process.env.PORT || 5000
+const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config({ path: "../.env" });
+const connectDB = require("../backend/config/db");
+const adminRoutes = require("./routes/adminRoutes");
+const hwRoutes = require("./routes/hwRoutes");
+const doctorRoutes = require("./routes/doctorRoutes");
+const familyRoutes = require("./routes/familyRoutes");
+const patientRoutes = require("./routes/PatientRoutes");
+const logRoutes = require("./routes/logRoutes");
+const prescriptionRoutes = require("./routes/prescriptionRoutes");
+const medicineRoutes = require("./routes/medicineRoutes");
+const path = require("path");
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5000;
 
 connectDB();
-const cors = require('cors')
-const http = require('http')
-const {Server} = require('socket.io');
+const cors = require("cors");
+const http = require("http");
+const { Server } = require("socket.io");
 
 console.log(PORT);
 const server = http.createServer(app);
-const io = new Server(server,{
-    cors:{
-        origin:"https://telemedicine-main.onrender.com/",
-        // origin:"http://localhost:3000",
-    },
-})
+const io = new Server(server, {
+  cors: {
+    // origin:"https://telemedicine-main.onrender.com/",
+    origin: "http://localhost:3000",
+  },
+});
 
-io.on('connection',(socket)=>{
-    console.log('Socket ID is'+socket.id)
+io.on("connection", (socket) => {
+  console.log("Socket ID is" + socket.id);
 
-    socket.on('send-message',(msg,room)=>{
-        if(socket.rooms.has(room)){
-            socket.to(room).emit('recieve-message',msg)
-        }
-    })
-    socket.on('join-room',(room,userId)=>{
-        // console.log(room);
-        socket.join(room);
-        socket.to(room).emit('user-connected',userId);
-    })
-    socket.on('leave-room',(room,userId) => {
-        socket.leave(room);
-        console.log('leaving room with id' + socket.id)
-        socket.to(room).emit('user-disconnected',userId)
-    })
-})
+  socket.on("send-message", (msg, room) => {
+    if (socket.rooms.has(room)) {
+      socket.to(room).emit("recieve-message", msg);
+    }
+  });
+  socket.on("join-room", (room, userId) => {
+    // console.log(room);
+    socket.join(room);
+    socket.to(room).emit("user-connected", userId);
+  });
+  socket.on("leave-room", (room, userId) => {
+    socket.leave(room);
+    console.log("leaving room with id" + socket.id);
+    socket.to(room).emit("user-disconnected", userId);
+  });
+});
 
+app.use(cors());
+app.use(express.json());
 
-app.use(cors())
-app.use(express.json())
-
-app.use('/api/admin',adminRoutes);
-app.use('/api/hw',hwRoutes);
-app.use('/api/doctor',doctorRoutes);
-app.use('/api/family',familyRoutes);
-app.use('/api/patient',patientRoutes);
-app.use('/api/logs',logRoutes)
-app.use('/api/prescription',prescriptionRoutes)
-app.use('/api/med',medicineRoutes)
-
+app.use("/api/admin", adminRoutes);
+app.use("/api/hw", hwRoutes);
+app.use("/api/doctor", doctorRoutes);
+app.use("/api/family", familyRoutes);
+app.use("/api/patient", patientRoutes);
+app.use("/api/logs", logRoutes);
+app.use("/api/prescription", prescriptionRoutes);
+app.use("/api/med", medicineRoutes);
 
 // app.get('/api',(req,res)=>{
 //     res.send("ok");
@@ -69,18 +67,15 @@ app.use('/api/med',medicineRoutes)
 
 const __dirname1 = path.resolve();
 
-if(process.env.Node_Env=='production'){
-    app.use(express.static(path.join(__dirname1,'./frontend/build')));
-    app.get('*',(req,res)=>{
-        res.sendFile(path.resolve(__dirname1,"./frontend/build/index.html"))
-    })
-}
-else{
-    
-    app.get('/',(req,res)=>{
-        res.send('ok');
-    })
+if (process.env.Node_Env == "production") {
+  app.use(express.static(path.join(__dirname1, "./frontend/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname1, "./frontend/build/index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("ok");
+  });
 }
 
-
-server.listen(PORT,console.log("server at " + PORT))
+server.listen(PORT, console.log("server at " + PORT));
